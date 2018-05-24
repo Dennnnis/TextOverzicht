b8f6834466def08eaf32d2dedfd9f9fcbce2970d
Dennnnis <dennisbeckertim@gmail.com>
2018-05-24 17:34:50 +0200

    Add project files.

---
+++ TextOverzicht/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace TextOverzicht
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(65, 20);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Select";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(83, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(596, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*\"";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.Font = new System.Drawing.Font("Consolas", 18.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.ItemHeight = 29;
+            this.listBox1.Items.AddRange(new object[] {
+            "   "});
+            this.listBox1.Location = new System.Drawing.Point(12, 51);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.None;
+            this.listBox1.Size = new System.Drawing.Size(667, 497);
+            this.listBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(691, 575);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.MinimumSize = new System.Drawing.Size(700, 600);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Text overzicht";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ TextOverzicht/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+
+namespace TextOverzicht
+{
+
+
+    public partial class Form1 : Form
+    {
+        public class CTuple
+        {
+            public CTuple(char item1, int item2)
+            {
+                Item1 = item1;
+                Item2 = item2;
+            }
+            public char Item1 { get; set; }
+            public int Item2 { get; set; }
+        }
+
+        int chars = 0;
+
+        List<CTuple> keyList = new List<CTuple>();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            textBox1.Text = openFileDialog1.FileName;
+
+            if (!openFileDialog1.CheckFileExists)
+            {
+                return;
+            }
+            if (textBox1.Text.Length < 1)
+            {
+                return;
+            }
+
+            keyList = new List<CTuple>();
+            chars = 0;
+            ReadFile();
+            DisplayText();
+        }
+
+        private void ReadFile()
+        {
+            var reader = new StreamReader(textBox1.Text);
+
+            char read;    
+            while (!reader.EndOfStream)
+            {
+                read = (char)reader.Read();
+                chars++;
+
+                bool found = false;
+                for (int i = 0; i < keyList.Count; i++)
+                {
+                    if (keyList[i].Item1 == read)
+                    {
+                        keyList[i].Item2 += 1;
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (!found)
+                {
+                    keyList.Add(new CTuple(read,1));
+                }
+            }
+        }
+
+        private decimal Proc(decimal a, decimal b)
+        {
+
+            return (b / a) * 100;
+        }
+
+        private string Stretch(string target, int size)
+        {
+            if (target.Length > size)
+            {
+                return target.Substring(0, size);
+            }
+            else
+            {
+                while (target.Length < size)
+                {
+                    target += " ";
+                }
+                return target;
+            }
+        }
+
+        private string SStretch(string target, int size)
+        {
+            if (target.Length > size)
+            {
+                return target.Substring(0, size-1) + ".";
+            }
+            else
+            {
+                while (target.Length < size)
+                {
+                    target += " ";
+                }
+                return target;
+            }
+        }
+
+
+        private void DisplayText()
+        {
+            listBox1.Items.Clear();
+
+            listBox1.Items.Add("File name:  " + textBox1.Text);
+            listBox1.Items.Add("Characters: " + chars);
+            listBox1.Items.Add("");
+            listBox1.Items.Add(" |Percent|  |Amount|  |Character|");
+
+
+            keyList.Sort((x, y) => y.Item2.CompareTo(x.Item2));
+
+            foreach (var c in keyList)
+            {
+                listBox1.Items.Add(
+
+                    " " + Stretch(Convert.ToString(Proc(chars, c.Item2)), 5) + "%   " +
+
+                    "  (" + SStretch( Convert.ToString(c.Item2) , 6) + ")" +
+
+                    "  \"" + c.Item1 + "\""
+                    );
+            }
+        }
+    }
+}
